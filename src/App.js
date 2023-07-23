a8582bce9ef3c97ba8ad58183fba098f14b03730
vicenzo <deeprajm35@gmail.com>
2023-07-23 15:36:14 +0530

    Imdb clone

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
+import NotFound from "./components/notFound/NotFound";
+
+function App() {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("movies/api/v1/all");
+
+      setMovies(response.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`movies/api/v1/${movieId}`);
+      console.log(response);
+      const singleMovie = response.data.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviewIds);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/reviews/:movieId"
+            element={
+              <Reviews
+                
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
